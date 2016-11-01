bab1c5e41497e62e40a037d7112be5d464301f57
jesper <j.svensson.se@gmail.com>
2016-11-01 11:46:42 +0100

    dev-env

--- gui-flask/run.py
+++ gui-flask/run.py
@@ -1,16 +1,49 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+
+import gevent
+from gevent.wsgi import WSGIServer
+from gevent.queue import Queue
+import sse
 
 app = Flask(__name__)
-mysql = MySQL(app)
-
+subscriptions = []
 
 @app.route('/')
 def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    return str(rv)
+    return "hello"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/publish")
+def publish():
+    #Send dummy data
+    def notify():
+        msg = '{"player1": "Linus",'\
+            '"player2": "Masse",'\
+            '"score": "(10, 0)"}'
+        for sub in subscriptions[:]:
+            sub.put(msg)
+
+    gevent.spawn(notify)
+    return "OK"
+
+
+@app.route("/subscribe")
+def subscribe():
+    def gen():
+        q = Queue()
+        subscriptions.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = ServerSentEvent(str(result))
+                yield ev.encode()
+        except GeneratorExit:  # Or maybe use flask signals
+            subscriptions.remove(q)
+
+    return Response(gen(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    server = WSGIServer(("0.0.0.0", 5000), app)
+    server.serve_forever()
+
