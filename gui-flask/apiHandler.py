6e8e1dc5ca1ff9cd67901dcd913ae87b2901583b
kevintatou <kevintatou94@gmail.com>
2016-11-01 14:54:55 +0100

    regsiter and get done

--- gui-flask/apiHandler.py
+++ gui-flask/apiHandler.py
@@ -1,14 +1,39 @@
 
-from flask import render_template, Flask
+from flask import render_template, Flask, request, redirect
 
-import requests
+import config
+import json
 
 app = Flask(__name__)
-@app.route('/users/')
-def users():
-    req = requests.get('https://api.github.com/events')
-    return req
+@app.route('/users/<card_id>')
+def users(card_id):
+    query = "SELECT * FROM user WHERE cardID=%s"
+    config.cur.execute(query, (card_id,))
+    res = config.cur.fetchall()
+    json_result = {
+        "name" : res[0][1],
+        "card_id": res[0][2],
+        "wins": res[0][3],
+        "losses": res[0][4],
+        "totalgames": res[0][5]
+    }
+    return json.dumps(json_result)
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form["name"]
+        card_id = request.form["card_id"]
+        query = "INSERT INTO user (name, cardID) VALUES (%s, %s)"
+        config.cur.execute(query, (name, card_id))
+        config.db.commit()
+        print(query)
+        return "Registration successful"
+    else: 
+        return "nej"
+
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
--- gui-flask/config.py
+++ gui-flask/config.py
@@ -1,5 +1,5 @@
 import mysql
 from mysql import connector
 
-db = connector.connect(host="localhost", user="root", password="", db="skatefest")
+db = connector.connect(host="localhost", user="root", password="", db="pingishack")
 cur = db.cursor()
